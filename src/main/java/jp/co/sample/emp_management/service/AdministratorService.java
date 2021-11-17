d2de6ac17a7d381ae941502224f996331da96b66
miyazawa <nami.miyazawa@rakus-partners.co.jp>
2021-11-17 10:56:59 +0900

    (1-5)中級 確認用パスワード：確認用パスワード欄を追加

--- src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
+++ src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
@@ -2,6 +2,7 @@ package jp.co.sample.emp_management.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
--- src/main/resources/static/js/insert.js
+++ src/main/resources/static/js/insert.js
@@ -5,4 +5,28 @@ $(function(){
 		$('#touroku').prop('disabled',true);
 		$('form').submit();
 	})
+	
+	$(document).on('keyup', '#checkPassword', function(){
+		let hostUrl = 'http://localhost:8080/check';
+		let inputPass = $('#password').val();
+		let inputCheckPass = $('#checkPassword').val();
+		$.ajax({
+			url:hostUrl,
+			type:'POST',
+			dataType:'json',
+			data:{
+				password:inputPass,
+				checkPassword:inputCheckPass},
+			async:true
+		}).done(function(data){
+			console.log(data);
+			console.dir(JSON.stringify(data));
+			$('#checkMessage').text(data.checkMessage);
+		}).fail(function(XMLHttpRequest, textStatus, errorThrown){
+			alert('エラーが発生しました');
+			console.log('XMLHttpRequest:'+XMLHttpRequest.status);
+			console.log('textStatus:'+textStatus);
+			console.log('errorThrown:'+errorThrown.message);
+		})
+	})
 })
